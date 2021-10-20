768f9aed73fa411f3899902e203cffb8c64631f7
Home <liberate.tutemae1@gmail.com>
2021-10-20 17:05:04 +0700

    start of progect

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 "use strict";
 
-alert(13%4);
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотели?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("Один из последних проссмотренных фильмов?", ""),
+    b =  prompt("На сколько оцените его?", ""),
+    c = prompt("Один из последних проссмотренных фильмов?", ""),
+    d =  prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
